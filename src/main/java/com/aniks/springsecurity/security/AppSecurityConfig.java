0117e709492549d8b6e5e6dfdcbba13a432384f9
Aniks68 <ikechukwu.anene@decagon.dev>
2021-12-21 21:28:47 +0100

    third commit for creating appsecurity configuration class switching to basic auth

---
+++ src/main/java/com/aniks/springsecurity/security/AppSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.aniks.springsecurity.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests() // authorise requests
+                .anyRequest() // applies to any request
+                .authenticated() // client must authenticate by supplying username and password
+                .and() //
+                .httpBasic(); //    the form of enforcing the authencity is by basic auth.
+    }
+}
