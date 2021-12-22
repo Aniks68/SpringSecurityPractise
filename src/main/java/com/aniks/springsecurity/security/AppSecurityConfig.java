8f6d11997ca8ff06eb2e55192e12e9e9da5d88c9
Aniks68 <ikechukwu.anene@decagon.dev>
2021-12-22 09:32:43 +0100

    fifth commit for creating user and password & learning the need for encoding password through password encoder

--- src/main/java/com/aniks/springsecurity/security/AppSecurityConfig.java
+++ src/main/java/com/aniks/springsecurity/security/AppSecurityConfig.java
@@ -1,13 +1,28 @@
 package com.aniks.springsecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AppSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -18,5 +33,17 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated() // client must authenticate by supplying username and password
                 .and() //
                 .httpBasic(); //    the form of enforcing the authencity is by basic auth.
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails fabioUser = User.builder()
+                .username("fabio")
+                .password(passwordEncoder.encode("12345"))
+                .roles("STUDENT") //    spring identifies this as ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(fabioUser);
     }
 }
