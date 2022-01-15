0b247b7238004e1a450d7e76196dca321b3cb01c
Aniks68 <ikechukwu.anene@decagon.dev>
2022-01-15 14:40:45 +0100

    twenty-eight commit for getting JwtConfig data properly.

--- src/main/java/com/aniks/springsecurity/jwt/JwtConfig.java
+++ src/main/java/com/aniks/springsecurity/jwt/JwtConfig.java
@@ -1,21 +1,29 @@
 package com.aniks.springsecurity.jwt;
 
-import io.jsonwebtoken.security.Keys;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
-
+//@Configuration
+//@ConstructorBinding
+@Component
 @ConfigurationProperties(prefix = "application.jwt")
+@NoArgsConstructor
 public class JwtConfig {
 
+    @Value("${application.jwt.secretKey}")
     private String secretKey;
+
+    @Value("${application.jwt.tokenPrefix}")
     private String tokenPrefix;
+
+    @Value("${application.jwt.tokenExpirationAfterDays}")
     private Integer tokenExpirationAfterDays;
 
-    public JwtConfig() {
-    }
 
     public String getSecretKey() {
         return secretKey;
